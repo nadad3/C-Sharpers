7dd261c0a6d53b7620db7b554cc7972876ab584a
nadad3 <86581535+nadad3@users.noreply.github.com>
2021-12-04 16:36:44 -0500

    Update FMain.cs

--- ProjectA/FMain.cs
+++ ProjectA/FMain.cs
@@ -12,12 +12,12 @@ namespace ProjectA
 {
     public partial class FMain : Form
     {
-        public FMain()
+        BindingList<Movies> masterMovieList = new BindingList<Movies>();
+        public Form1()
         {
             InitializeComponent();
         }
-        List<Movies> masterMovieList = new List<Movies>();
-
+        
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult answer = MessageBox.Show("Are you sure you want to exit?",
@@ -32,16 +32,13 @@ namespace ProjectA
             About aboutForm = new About();
             aboutForm.ShowDialog();
         }
+       
 
-        private void refreshListToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            DbOps.RefreshMovies(masterMovieList);
-        }
+       
 
         private void Form1_Load(object sender, EventArgs e)
         {
             DataGridViewMovies.DataSource = masterMovieList;
-
             DataGridViewMovies.Columns["Id"].Visible = false;
             DataGridViewMovies.Columns["Genre"].Visible = false;
             DataGridViewMovies.Columns["TotalEarned"].Visible = false;
@@ -53,6 +50,48 @@ namespace ProjectA
 
 
             //load movies 
+            DbOps.RefreshingMovies(masterMovieList);
+        }
+
+        private void DataGridViewMovies_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = DataGridViewMovies.SelectedRows[0];
+            string movieTitle = Convert.ToString(row.Cells[1].Value);
+           FUpdate updateForm = new FUpdate(movieTitle);
+            updateForm.ShowDialog();
+            DbOps.RefreshMovies(masterMovieList);
+        }
+
+        private void deleteMovieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //open up a new delete form
+            FDeleted deleteForm = new FDeleted();
+            deleteForm.ShowDialog();
+            //Refresh the list in case some movies were chenged
+            DbOps.RefreshMovies(masterMovieList);
+        }
+
+        private void refreshListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DbOps.RefreshingMovies(masterMovieList);
+
+        }
+
+        private void addMovieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Show the Add form modally
+            AddingNewEventArgs addForm = new AddingNewEventArgs();
+            addForm.ShowDialog();
+            //Refresh the list in case some movies were added
+            DbOps.RefreshMovies(masterMovieList);
+        }
+
+        private void updateMovieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //open up a new update form
+            FUpdate updateForm = new FUpdate();
+            updateForm.ShowDialog();
+             //Refresh the list in case some movies were changed
             DbOps.RefreshMovies(masterMovieList);
         }
     }
